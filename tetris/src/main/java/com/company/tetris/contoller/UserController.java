62003729a3f5d399347e28deb5c399cca6576a8e
dpflaalee <dpflaalee@naver.com>
2025-08-21 01:32:24 +0900

    Update UserController.java

--- tetris/src/main/java/com/company/tetris/contoller/UserController.java
+++ tetris/src/main/java/com/company/tetris/contoller/UserController.java
@@ -12,13 +12,19 @@ import com.company.tetris.service.UserService;
 
 @Controller
 public class UserController {
-	private UserService userService = new UserService();
+
+	@Autowired
+    private UserService userService;
 	
 	@GetMapping("/login") public String showLoginPage() {return "login";}
 	
 	@PostMapping("/login") public String login(@RequestParam String userId, @RequestParam String password, HttpSession session, Model model) {
-		if(userService.validdateUser(userId, password)) {session.setAttribute("user", userId); return "나중에만들어질메인페이지";}
-		else {model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다."); return "login";}
+		if (userService.validateUser(userId, password)) {
+            User user = userService.getUser(userId);
+            session.setAttribute("user", user); //유저정보 저장
+			
+            return "메인 페이지 만들면 바꿔야해";
+        } else { model.addAttribute("error", "아이디 또는 비밀번호가 잘못되었습니다."); return "login"; }
 	}
 	
 	@GetMapping("/logout")public String logout(HttpSession session) {session.invalidate(); return "redirect:/login";}
