ab1a3e0a7024502b486f74d0c9960da9358223f1
dpflaalee <dpflaalee@naver.com>
2025-08-26 17:52:33 +0900

    250826 7bag 설정은 했는데 적용이 안된 것 같음

--- simpleTetris/src/main/java/com/company/tetris/logic/TetrominoGenerator.java
+++ simpleTetris/src/main/java/com/company/tetris/logic/TetrominoGenerator.java
@@ -1,14 +1,35 @@
 package com.company.tetris.logic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 //랜덤 생성
 public class TetrominoGenerator {
-	private static final char[] TYPES = {'I', 'O', 'T', 'S', 'Z', 'J', 'L'};
-	private Random rand = new Random();
+//	private static final char[] TYPES = {'I', 'O', 'T', 'S', 'Z', 'J', 'L'};
+//	private Random random = new Random();
+//	
+//	public Tetromino next() {
+//		char type = TYPES[random.nextInt(TYPES.length)];
+//		return new Tetromino(type);
+//	}
+	
+	private List<Character> bag = new ArrayList<>();
+	private Random random = new Random();
 	
 	public Tetromino next() {
-		char type = TYPES[rand.nextInt(TYPES.length)];
+		if(bag.isEmpty()) refillBag();
+		char type = bag.remove(0);
 		return new Tetromino(type);
+	}
+	
+	private void refillBag() {
+//		bag = Arrays.asList('I', 'O', 'T', 'S', 'Z', 'J', 'L');
+//		Collections.shuffle(bag, random);
+	    List<Character> types = Arrays.asList('I', 'O', 'T', 'S', 'Z', 'J', 'L');
+	    bag = new ArrayList<>(types); 
+	    Collections.shuffle(bag, random);
 	}
 }
--- simpleTetris/src/main/resources/static/js/game.js
+++ simpleTetris/src/main/resources/static/js/game.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     const canvas = document.getElementById("gameCanvas");
     const ctx = canvas.getContext("2d");
-    //let gameStarted = false;
+    const pauseResumeBtn = document.getElementById("pauseResumeBtn");
+    const startRestartBtn = document.getElementById("startRestartBtn");
     
     function draw(grid, currentBlocks){
         if(!grid || !grid.length) return;
@@ -23,26 +24,67 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
     function update(){
         fetch("/tetris/state")
-            .then(res=>res.json())
+            .then(res => res.json())
             .then(data=>{
                 draw(data.board, data.current);
                 document.getElementById("score").innerText = data.score;
-                // console.log(data.gameOver)
-                if(data.gameOver){document.getElementById("gameOverMessage").style.display="block";
-                }else{document.getElementById("gameOverMessage").style.display="none";}
+
+                // GAME OVER 
+                if (data.gameOver) { document.getElementById("gameOverMessage").style.display = "block";
+                } else { document.getElementById("gameOverMessage").style.display = "none"; }
+
+                //버튼 상태
+                if(data.current.length===0 || data.gameOver){
+                    startRestartBtn.innerText="게임 시작";
+                    pauseResumeBtn.disabled = true;
+                }else{
+                    startRestartBtn.innerText = "재시작";
+                    pauseResumeBtn.disabled=false;
+                    //console.log("paused 상태:", data.paused);
+                    pauseResumeBtn.innerText=data.paused ? "게임 재개" : "일시 정지";
+                }
             });
     }
-    document.addEventListener("keydown", e =>{
-        let direction = null;
-        if(e.key === "ArrowLeft") direction ="left";
-        if(e.key === "ArrowRight") direction = "right";
-        if(e.key === "ArrowDown") direction = "down";
-        if(e.key === "ArrowUp") direction = "rotate";
-        if(e.key === ""){
-            fetch("/tetris/drop", {method: "POST"}).then(update); return;
-        }
-        if(direction){fetch("/tetris/move?direction="+direction, {method:"POST"}).then(update);}
+    
+    //일시정지-재개
+    pauseResumeBtn.addEventListener("click",()=>{
+        fetch("/tetris/state")
+            .then(res=>res.json())
+            .then(data=>{
+                const url = data.paused ? "/tetris/resume" : "/tetris/pause";
+                fetch(url, {method:"POST"}).then(()=>{update();});
+            });
     });
+
+    //시작-재시작
+    startRestartBtn.addEventListener("click", ()=>{
+        fetch("/tetris/state")
+            .then(res=>res.json())
+            .then(data=>{
+                const url = data.paused ? "/tetris/spawn" : "/tetris/restart";
+                fetch(url, {method:"POST"}).then(update);
+            });
+    });
+
+    //키 조작
+    document.addEventListener("keydown", e =>{
+        fetch("/tetris/state")
+            .then(res=>res.json())
+            .then(data=>{
+                if(data.paused || data.gameOver) return;
+        
+                let direction = null;
+                if(e.key === "ArrowLeft") direction ="left";
+                if(e.key === "ArrowRight") direction = "right";
+                if(e.key === "ArrowDown") direction = "down";
+                if(e.key === "ArrowUp") direction = "rotate";
+                if(e.key === ""){
+                    fetch("/tetris/drop", {method: "POST"}).then(update); return;
+                }
+                if(direction){fetch("/tetris/move?direction="+direction, {method:"POST"}).then(update);}
+            });
+    });
+    
     //자동 낙하
     setInterval(()=>{
         fetch("/tetris/state")
@@ -54,26 +96,22 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 update();
             });
     },500);
-    // setInterval(()=>{
-    //     fetch("/tetris/move?direction=down",{method:"POST"});
-    //     update();
-    // },500); //일시정지, 재시작 적용 전
 
-    //게임 시작
-    document.getElementById("startBtn").addEventListener("click",()=>{
-        fetch("/tetris/spawn", {method:"POST"});
-    });
+    // //게임 시작
+    // document.getElementById("startBtn").addEventListener("click",()=>{
+    //     fetch("/tetris/spawn", {method:"POST"});
+    // });
 
-    //일시정지
-    document.getElementById("pauseBtn").addEventListener("click",()=>{
-        fetch("/tetris/pause",{method:"POST"});
-    });
-    //일시정지 해제
-    document.getElementById("resumeBtn").addEventListener("click",()=>{
-        fetch("/tetris/resume",{method:"POST"});
-    });
-    //재시작
-    document.getElementById("restartBtn").addEventListener("click",()=>{
-        fetch("/tetris/restart",{method:"POST"}).then(update);
-    });
+    // //일시정지
+    // document.getElementById("pauseBtn").addEventListener("click",()=>{
+    //     fetch("/tetris/pause",{method:"POST"});
+    // });
+    // //일시정지 해제
+    // document.getElementById("resumeBtn").addEventListener("click",()=>{
+    //     fetch("/tetris/resume",{method:"POST"});
+    // });
+    // //재시작
+    // document.getElementById("restartBtn").addEventListener("click",()=>{
+    //     fetch("/tetris/restart",{method:"POST"}).then(update);
+    // });
 });
